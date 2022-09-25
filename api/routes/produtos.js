937d7be416d79632881499b643c1faec24f54e76
MiletoGuy <tales5c@outlook.com>
2022-09-25 12:14:06 -0300

    Rotas produtos e produto/:id

--- api/routes/produtos.js
+++ api/routes/produtos.js
@@ -10,9 +10,41 @@ const pool = new Pool({
 });
 
 router.get('/', (req, res, next) => {
-    res.status(200).send({
-        mensagem: "Retorna todos os produtos"
+    let promise = new Promise(function (resolve, reject) {
+        pool.query('SELECT * FROM produto', [], (error, result) => {
+            if (error) {
+                console.log("erro")
+                reject("Ocorreu um erro!", error)
+            } else {
+                console.log("ok")
+                resolve(result)
+            }
+        })
     })
+    promise.then(result => {
+        const response = {
+            quantidade: result.rowCount,
+            produtos: result.rows.map(prod => {
+                return {
+                    id: prod.id,
+                    nome: prod.nome,
+                    valor: prod.valor,
+                    emEstoque: prod.emEstoque,
+                    fornecedor: prod.fornecedor,
+                    request: {
+                        tipo: 'GET',
+                        descricao: 'Trás todos os produtos',
+                        url: 'http://localhost:3001/produtos/' + prod.id
+                    }
+                }
+            })
+        }
+        res.status(200).send({response})
+    })
+        .catch(error =>
+            res.status(400).send({mensagem: "ocorreu um erro", error})
+        )
+
 })
 
 router.post('/', (req, res, next) => {
@@ -32,7 +64,7 @@ router.post('/', (req, res, next) => {
 
     promise.then(
         (result) => {
-            res.status(201).send({mensagen: "Produto adicionado"})
+            res.status(201).send({mensagen: "Produto adicionado"})//teste
             console.log(result)
         },
         (error) => {
@@ -44,31 +76,46 @@ router.post('/', (req, res, next) => {
 router.get('/:id_produto', (req, res, next) => {
     let promise = new Promise(function (resolve, reject) {
         const id_produto = req.params.id_produto
-        console.log("O id é: ", id)
         pool.query('SELECT * FROM produto WHERE id = $1', [id_produto], (error, result) => {
             if (error) {
+                console.log("erro")
                 reject("Ocorreu um erro!", error)
+            } else {
+                console.log("ok")
+                resolve(result)
             }
-            resolve("Deu certo!", result)
         })
+    })
 
-        promise.then(
-            (result) => {
-                res.status(201).send({resultado: result})
-                console.log(result)
-            },
-            (error) => {
-                res.status(400).send({mensagem})
+    promise.then(result => {
+        const response = {
+            produto: result.rows.map(row => {
+                return {
+                    id: row.id,
+                    nome: row.nome,
+                    valor: row.valor,
+                    emEstoque: row.emEstoque,
+                    fornecedor: row.fornecedor
+                }
+            }),
+            request: {
+                tipo: 'GET',
+                descricao: 'Trás um produto especifico',
+                url: 'http://localhost:3001/produtos/' + result.rows[0].id
             }
-        )
+        }
+        res.status(200).send({response})
     })
+        .catch(error => res.status(400).send({mensagem: "ocorreu um erro", error}))
+
+
 })
 
-router.patch('/', (req, res, next) => {
-    res.status(200).send({
-        mensagem: "Produto alterado"
-    })
-})
+// router.patch('/', (req, res, next) => {
+//     res.status(200).send({
+//         mensagem: "Produto alterado"
+//     })
+// })
 
 router.delete('/', (req, res, next) => {
     res.status(200).send({
